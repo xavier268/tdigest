5ab0dabad5d84eccbad5d74106743029326132e4
xavier <xavier@fed29onDellXPS>
2019-09-18 13:23:25 +0200

    more tests

--- stats_test.go
+++ stats_test.go
@@ -1,6 +1,7 @@
 package tdigest
 
 import (
+	"math"
 	"testing"
 )
 
@@ -21,9 +22,9 @@ func SetUp1() *TD {
 	}
 	sz = ForceMinMax(sz)
 	td := NewTD(sz)
-	for i := 1; i < 1000000; i++ {
-		td.Add(float64((i * 987897) % 1000000))
-		if i%1000 == 0 {
+	for i := 0; i < 10000000; i++ {
+		td.Add(float64((i * 98013) % 1000000))
+		if i%1000 == 0 { // Limit memory footprint
 			td.Digest()
 		}
 	}
@@ -43,8 +44,12 @@ func TestBasicStats(t *testing.T) {
 func TestStat(t *testing.T) {
 	td := SetUp1()
 	td.Dump()
-	if td.Count() != 999999 || td.Min() != 1.0 || td.Max() != 999999.0 || td.Mean() != 500000. {
+	if td.Count() != 10000000 ||
+		math.Abs(td.Min()) > 0.00001 ||
+		math.Abs(td.Max()-999999.0) > 0.00001 ||
+		math.Abs(td.Mean()-499999.5) > 0.00001 {
 		td.Dump()
 		t.FailNow()
 	}
+
 }
