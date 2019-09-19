5d666cf85d42fb642a0862023d6f5cc2659b93e0
xavier <xavier@fed29onDellXPS>
2019-09-19 19:45:59 +0200

    doc

--- sizer.go
+++ sizer.go
@@ -4,22 +4,18 @@ package tdigest
 // Assumptions are that 0 <= q <= 1.0
 type Sizer func(n int, q float64) float64
 
-// compiler checks
-var _ Sizer = LinearSizer
-var _ Sizer = PolySizer
-
 // NilSizer : When sizer is not set, use this value.
 var NilSizer Sizer = MakeConstSizer(1)
 
 // PolySizer defines a polynom structure for the sizer
 // Use ScaleSizer to scale this to the target ,nbr of buckets.
-func PolySizer(n int, q float64) float64 {
+var PolySizer Sizer = func(n int, q float64) float64 {
 	return float64(n) * q * (1 - q)
 }
 
 // LinearSizer will size buckets linearly, ending up with appx 2 to 3  buckets.
 // Use scale to adjust actula number of buckets.
-func LinearSizer(n int, _ float64) float64 {
+var LinearSizer Sizer = func(n int, _ float64) float64 {
 	return float64(n) / 2.
 }
 
